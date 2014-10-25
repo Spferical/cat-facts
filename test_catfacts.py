a5bcc361b641211e0d5bb397b2bdcf017261fc49
Matthew Pfeiffer <spferical@gmail.com>
2014-10-25 17:40:47 -0400

    Update tests for renamed function

--- test_catfacts.py
+++ test_catfacts.py
@@ -6,12 +6,12 @@ import os
 
 
 class TestCatFacts(unittest.TestCase):
-    def test_remove_lines_containing_text_from_file(self):
+    def test_remove_matching_lines_from_file(self):
         f = open('tmp.txt', 'w')
         f.write('asdf 1234\n')
         f.write('fdsa 4321\n')
         f.close()
-        catfacts.remove_lines_containing_text_from_file('asdf', 'tmp.txt')
+        catfacts.remove_matching_lines_from_file('asdf', 'tmp.txt')
         f = open('tmp.txt', 'r')
         lines = f.readlines()
         try:
