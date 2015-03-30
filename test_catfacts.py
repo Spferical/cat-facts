2d7e0993825db2c1d71ad760761b208a7550ac90
Matthew Pfeiffer <spferical@gmail.com>
2015-03-30 15:48:22 -0400

    Formatting

--- test_catfacts.py
+++ test_catfacts.py
@@ -6,18 +6,19 @@ import os
 
 
 class TestCatFacts(unittest.TestCase):
+
     def test_remove_matching_lines_from_file(self):
-        f = open('tmp.txt', 'w')
-        f.write('asdf 1234\n')
-        f.write('fdsa 4321\n')
-        f.close()
+        temp_file = open('tmp.txt', 'w')
+        temp_file.write('asdf 1234\n')
+        temp_file.write('fdsa 4321\n')
+        temp_file.close()
         catfacts.remove_matching_lines_from_file('asdf', 'tmp.txt')
-        f = open('tmp.txt', 'r')
-        lines = f.readlines()
+        temp_file = open('tmp.txt', 'r')
+        lines = temp_file.readlines()
         try:
             self.assertEqual(lines, ['fdsa 4321\n'])
         finally:
-            f.close()
+            temp_file.close()
             os.remove('tmp.txt')
 
     def test_get_command_from_text(self):
@@ -32,14 +33,14 @@ class TestCatFacts(unittest.TestCase):
             'Hourly': ('hourly', []),
             'hourly': ('hourly', []),
             'asdjflas huf als dnflhourly': ('hourly', []),
-            'invite sms 5551234567 verizon': ('invite', ['sms', '5551234567',
-                                              'verizon']),
+            'invite sms 5551234567 verizon':
+                ('invite', ['sms', '5551234567', 'verizon']),
             'invite sms foo bar': ('invite', []),
-            'invite email foo@example.com': ('invite', ['email',
-                                             'foo@example.com']),
+            'invite email foo@example.com':
+                ('invite', ['email', 'foo@example.com']),
             '[][] invite email (#*&)u90U()#*&)(&*)@(@)@ extra_text[]]]':
                 ('invite', ['email', '(#*&)u90u()#*&)(&*)@(@)@']),
-            '!)(*#$   invite sms 1231231231 AT&t 9102018)(*)*)' :
+            '!)(*#$   invite sms 1231231231 AT&t 9102018)(*)*)':
                 ('invite', ['sms', '1231231231', 'at&t']),
             'invite': ('invite', []),
             'invite email': ('invite', []),
